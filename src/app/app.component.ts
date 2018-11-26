27a4452be2a85a0b8d8ca534725ee314cca6cf04
joris.duenas <joris.duenas@gmail.com>
2018-11-26 16:38:49 +0100

    add: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pizzamania';
+  title = 'Pizza Mania !';
+  monStyle = '';
 }
